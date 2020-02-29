ea9bd7e3c8b52bad0f278e1e442eb7c4312992dd
Charan <charan.r@pacewisdom.com>
2020-02-29 23:08:18 +0530

    Updates

---
+++ precache-manifest.d04858cfbe8320040c1b3e4de90c35e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e92722930597c0e34d8e41a3e51a3f5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "8b0b85450263fea9d350",
+    "url": "/react-gh-pages/static/css/main.40a6f459.chunk.css"
+  },
+  {
+    "revision": "7beb8f7a19ccadb8aa01",
+    "url": "/react-gh-pages/static/js/2.1f26c4fd.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/react-gh-pages/static/js/2.1f26c4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0b85450263fea9d350",
+    "url": "/react-gh-pages/static/js/main.fa72cade.chunk.js"
+  },
+  {
+    "revision": "6a9842ec7f039ba73961",
+    "url": "/react-gh-pages/static/js/runtime-main.563b5616.js"
+  }
+]);
